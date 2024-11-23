6e1120fd4c0002c1149d2de88117a721e9aefa59
oussamalkd <oussamalkd99@gmail.com>
2024-11-23 12:51:45 +0100

    enable ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: 'static',
-  ssr: false,
+  target: 'server',
+  ssr: true,
   head: {
     title: 'cure-near-me',
     htmlAttrs: {
